9c8e9cf855f692471c24967e44644c1b8f635395
rudykocur <grzegorz.przydryga@gmail.com>
2017-01-09 17:16:02 +0100

    missing job implementation :)

--- src/jobs.mineralToStorage.js
+++ src/jobs.mineralToStorage.js
@@ -8,10 +8,29 @@ class MineralToStorageJobGenerator extends JobGenerator {
     constructor(handler) {
         super(handler);
 
-        this.interval = 17;
+        this.interval = 12;
     }
 
     generateJobs() {
+        /** @type Mineral */
+        var mineral = _.first(this.room.find(FIND_MINERALS));
+        var jobs = this.state.jobs;
+
+        let storage = this.room.getStorage();
+
+        var container = _.first(mineral.pos.findInRange(this.room.getContainers(), 1));
+
+        var key = `mineralMove-${mineral.mineralType}`;
+        if(container && container.store[mineral.mineralType] > 400) {
+            if(!(key in jobs)) {
+                jobs[key] = this._getJobTransferDict(key, container, storage, mineral.mineralType);
+            }
+
+            jobs[key].amount = container.store[mineral.mineralType];
+        }
+        else {
+            delete jobs[key];
+        }
     }
 }
 
